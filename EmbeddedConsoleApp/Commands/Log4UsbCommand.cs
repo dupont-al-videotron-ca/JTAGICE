7b853385b4ef4d04f551cbd675f975cdc83f4162
Alain Dupont <dupont.al@videotron.ca>
2026-07-16 12:27:59 -0400

    Add EmbeededConsoleApp Folder
    - Add Log4UsbCommand
    - add necessary files for project.

---
+++ EmbeddedConsoleApp/Commands/Log4UsbCommand.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentArgs;
+using FluentArgs.Help;
+using log4net.Core;
+using Log4UsbService;
+
+{9
+    internal class Log4UsbCommand : MyFramework.CommandArgs.CommandArgsBase
+    {
+
+
+        #region Constructors 
+        public Log4UsbCommand() : base(_commandName)
+        {
+            Create();
+        }
+
+        #endregion
+
+
+        #region Fields 
+
+        private const string _commandName = "log4usb";
+
+        #endregion
+
+
+        #region Properties 
+
+        public Level Level { get; private set; } = Level.Off;
+
+
+        #endregion
+
+
+        #region Delegates / Events 
+
+        #endregion
+
+
+        #region Public Methods 
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="Log4UsbCommand"/> with configured argument parsing.
+        /// </summary>
+        /// <returns>A new <see cref="Log4UsbCommand"/> instance configured with:
+        /// <list type="bullet">
+        /// <item><description>Help flag support via -h</description></item>
+        /// <item><description>Optional level parameter (-l) with default value of <see cref="Level.Off"/></description></item>
+        /// <item><description>Disallowed unused arguments validation</description></item>
+        /// <item><description>Help and parsing error output directed to Console.Error</description></item>
+        /// </list>
+        /// </returns>
+        protected override void Create()
+        {
+
+                /* 1) General parser configurations: The ordering does not matter */
+            var parsable = ApplyCommonConfig(FluentArgsBuilder.New())
+
+                /* 2) Parse parameters, list-parameters, flags and commands. Parsing is done in the defined ordering. */
+                .Parameter<string>("-l", "-level")
+                    .WithExamples(Level.Off.Name, Level.Debug.Name, Level.Info.Name, Level.Warn.Name, Level.Error.Name, Level.Fatal.Name, Level.All.Name)
+                    .WithDescription($"The log level to use for embedded logging. Default is {Level.Off.Name}.")
+                    .WithValidation(a => a != null, $"The log level must be one of the following: {Level.Off.Name}, {Level.Debug.Name}, {Level.Info.Name}, {Level.Warn.Name}, {Level.Error.Name}, {Level.Fatal.Name}, {Level.All.Name}.")
+                    .IsOptionalWithDefault(Level.Off.Name)
+
+                /* 3) Positional parameters. Parsing is done in the defined ordering. */
+                .PositionalArgument<string>()
+                    .WithValidation(a => a == _commandName)
+                    //.WithExamples(_commandName)
+                    .WithDescription($"Use '{_commandName}' to activate or disable embedded logging.")
+                    .IsRequired()
+
+                /* 4) Load remaining arguments */
+                //.LoadRemainingArguments()
+
+
+                /* 5) Callback */
+                .Call(RemainingArg => LevelArg => 
+                {
+                    SetLevel(LevelArg);
+                })
+                .Build();
+
+            this.SetParsable(parsable);
+        }
+
+        private void SetLevel(string levelArg)
+        {
+            switch (levelArg.ToLower())
+            {
+                case "off":
+                    this.Level = Level.Off;
+                    break;
+                case "debug":
+                    this.Level = Level.Debug;
+                    break;
+                case "info":
+                    this.Level = Level.Info;
+                    break;
+                case "warn":
+                    this.Level = Level.Warn;
+                    break;
+                case "error":
+                    this.Level = Level.Error;
+                    break;
+                case "fatal":
+                    this.Level = Level.Fatal;
+                    break;
+                case "all":
+                    this.Level = Level.All;
+                    break;
+                default:
+                    throw new ArgumentException($"Invalid log level: {levelArg}");
+            }
+        }
+            
+        #endregion
+
+
+        #region Protected Methods 
+
+        #endregion
+
+        #region Private Methods 
+
+        #endregion
+
+        #region Private Classes / Enum 
+
+        #endregion
+    }
+}
---
+++ EmbeddedConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿
+namespace EmdeddedConsoleApp
+{
+    using System;
+    using System.Reflection;
+    using System.Threading.Tasks;
+    using EmbeddedConsoleApp.Commands;
+    using FluentArgs;
+    using FluentArgs.Help;
+    using log4net;
+    using log4net.Appender;
+    using log4net.Config;
+    using log4net.Core;
+    using log4net.Repository.Hierarchy;
+    using Log4UsbService.EmbeddedData;
+    using MyFramework.CommandArgs;
+
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            // initialize log4net
+            //Hierarchy logRepository = (Hierarchy)LogManager.GetRepository(Assembly.GetCallingAssembly());
+            //XmlConfigurator.ConfigureAndWatch(logRepository, new FileInfo("log4net.config"));
+
+            // initialize commands
+            new Log4UsbCommand();
+
+
+            // Run application console
+            Commands.RunConsole(args);
+
+            Console.WriteLine("Test complete. Hit enter");
+        }
+
+    }
+}
